975f97b273418fdeeabf3725feae4d15fe6336a8
bilw <william@shinyobjects.net>
2015-05-14 21:33:00 -0400

    added image handling

--- site/app/controllers/controllers.js
+++ site/app/controllers/controllers.js
@@ -11,11 +11,34 @@ steleApp.controller("mudrasController", function($scope, $firebaseArray, FIREBAS
 			title: $scope.adding_mudra.title,
 			wish: $scope.adding_mudra.wish,
 			email: $scope.adding_mudra.email,
-			timestamp: Firebase.ServerValue.TIMESTAMP
+			timestamp: Firebase.ServerValue.TIMESTAMP,
+			imageurl: imageUpped
 		});
 		$scope.adding_mudra = "";
 		$scope.myform.$setUntouched();
-		$scope.numLimit = 3;
 	}
+
+	var imageUpped;
+
+	$scope.addImage = function( new_image ){
+
+	  filepicker.setKey("AdPlL1v8nRQSF6poQO8nyz");
+
+	  filepicker.pick(
+	  {
+	    mimetypes: ['image/*', 'text/plain'],
+	    container: 'window',
+	    services:['COMPUTER'],
+	  },
+	  function(Blob){
+	    imageUpped = Blob.url;
+	    console.log(imageUpped);
+	  },
+	  function(FPError){
+	    console.log(FPError.toString());
+	  }
+	  );
+	}
+
 });
 
