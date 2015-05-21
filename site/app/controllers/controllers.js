4504b2da2dacb93bdfd8ac3e91b0f80c28fab36b
bilw <william@shinyobjects.net>
2015-05-21 18:41:49 -0400

    last minute commit still toiling away

--- site/app/controllers/controllers.js
+++ site/app/controllers/controllers.js
@@ -42,32 +42,34 @@ steleApp.controller('mudrasController', ['$scope', '$firebaseArray', 'FIREBASE_U
 
 }]);
 
-steleApp.controller('mudraViewController', ['$scope', '$firebaseArray', '$routeParams', 'FIREBASE_URL', function($scope, $firebaseArray, $routeParams,
-	FIREBASE_URL) {
+steleApp.controller('mudraViewController', ['$scope', '$routeParams', '$firebaseArray', '$firebaseObject', 'FIREBASE_URL', function($scope, $routeParams, $firebaseArray, $firebaseObject, FIREBASE_URL) {
 
-	// var ref = new Firebase(FIREBASE_URL);
-	// var postRef = ref.child('data');
-	// $scope.mudraData = $firebaseArray(ref);
+	// $scope.mudra_title = $routeParams.mudra_title;
+	// $scope.mudra_email = $routeParams.mudra_email;
+  	
+	// var mudra_title = $routeParams.mudra_title;
+	// var pushImg = new Firebase(FIREBASE_URL + '/' + mudra_title);
 
-	//$scope.mudra_title = $routeParams.mudra_title;
-	//var mudra_title = $routeParams.mudra_title;
-	//var mudraData = new Firebase(FIREBASE_URL + mudra_title);
+    //$scope.mudraData = $firebaseObject(pushImg);
 
-	//console.log(postRef.key()); // this points to the key and objects within on firebase
-
-	//console.log(postRef);
-
+    // console.log(mudra_title);
+    // console.log(pushImg);
 	
 	var ref = new Firebase(FIREBASE_URL);
 	$scope.mudraData = $firebaseArray(ref);
 	
 	ref.once('value', function(nameSnapshot) {
-	  var val = nameSnapshot.val();
+	  //var val = nameSnapshot.val();
+	  var key = nameSnapshot.key();
+	  var firstNameSnapshot = nameSnapshot.child('-JpJu-bqRvfXStpQ1QZE/email');
+	  var firstName = firstNameSnapshot.val();
 	  
-	  console.log(val);
+	  console.log(nameSnapshot.val());
+	  console.log(key);
+	  console.log(firstName);
+	  console.log(nameSnapshot.key());
 
 	});
-
 	
 
 }]);
